32a609065d6db776ab87e980fd1dae585fdb00ba
Jesper Olsson <jesper.olsson191@gmail.com>
2016-05-03 15:30:46 +0200

    created PolarVector

---
+++ src/PolarVector.java
@@ -0,0 +1,19 @@
+
+public class PolarVector {
+	private double r;
+	private double theta;
+	
+	public PolarVector(double r, double theta) {
+		this.r = r;
+		this.theta = theta;
+	}
+	
+	public double getR() {
+		return r;		
+	}
+	
+	public double getTheta() {
+		return theta;
+	}
+
+}
