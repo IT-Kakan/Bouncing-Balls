bb5dc1c2b760958c9def4cd8ab2b198635e507c4
Jesper Olsson <jesper.olsson191@gmail.com>
2016-05-03 16:39:02 +0200

    created RectVector

--- src/PolarVector.java
+++ src/PolarVector.java
@@ -15,5 +15,4 @@ public class PolarVector {
 	public double getTheta() {
 		return theta;
 	}
-
 }
---
+++ src/RectVector.java
@@ -0,0 +1,18 @@
+
+public class RectVector {
+	private double x;
+	private double y;
+	
+	public RectVector(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+}
